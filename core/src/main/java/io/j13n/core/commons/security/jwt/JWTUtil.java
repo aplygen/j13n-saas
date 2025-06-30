4d6c8f2c90ab4b3a250ed4f0c35bec85ed599a2a
lawbringr1 <vishwas.saini@modlix.com>
2025-07-01 00:31:18 +0530

    Upgraded to java 24

--- core/src/main/java/io/j13n/core/commons/security/jwt/JWTUtil.java
+++ core/src/main/java/io/j13n/core/commons/security/jwt/JWTUtil.java
@@ -4,20 +4,21 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import lombok.Builder;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
-import lombok.Builder;
-import reactor.util.function.Tuple2;
-import reactor.util.function.Tuples;
 
 public class JWTUtil {
 
-    private JWTUtil() {}
+    private JWTUtil() {
+    }
 
     public static Tuple2<String, LocalDateTime> generateToken(JWTGenerateTokenParameters params) {
 
@@ -25,28 +26,27 @@ public class JWTUtil {
 
         return Tuples.of(
                 Jwts.builder()
-                        .setIssuer("qplygen")
-                        .setSubject(params.userId.toString())
-                        .setClaims(new JWTClaims()
+                        .issuer("qplygen")
+                        .subject(params.userId.toString())
+                        .claims(new JWTClaims()
                                 .setUserId(params.userId)
                                 .setHostName(params.host)
                                 .setPort(params.port)
                                 .setOneTime(params.oneTime)
                                 .getClaimsMap())
-                        .setIssuedAt(Date.from(Instant.now()))
-                        .setExpiration(Date.from(Instant.now().plus(params.expiryInMin, ChronoUnit.MINUTES)))
-                        .signWith(Keys.hmacShaKeyFor(params.secretKey.getBytes()), SignatureAlgorithm.HS512)
+                        .issuedAt(Date.from(Instant.now()))
+                        .expiration(Date.from(Instant.now().plus(params.expiryInMin, ChronoUnit.MINUTES)))
+                        .signWith(Keys.hmacShaKeyFor(params.secretKey.getBytes()), Jwts.SIG.HS512)
                         .compact(),
                 expirationTime);
     }
 
     public static JWTClaims getClaimsFromToken(String secretKey, String token) {
 
-        JwtParser parser = Jwts.parserBuilder()
-                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+        JwtParser parser = Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
                 .build();
 
-        Jws<Claims> parsed = parser.parseClaimsJws(token);
+        Jws<Claims> parsed = parser.parseSignedClaims(token);
 
         return JWTClaims.from(parsed);
     }
