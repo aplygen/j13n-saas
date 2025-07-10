03781093392c65fb536d462060c43be50212a82f
lawbringr1 <vishwas.saini@modlix.com>
2025-07-11 00:18:37 +0530

    Fixed Docker Compose. Added Actuator endpoints

--- core/src/main/java/io/j13n/core/CoreApplication.java
+++ core/src/main/java/io/j13n/core/CoreApplication.java
@@ -4,8 +4,8 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 @EnableRabbit
+@EnableDiscoveryClient
 @ComponentScan(basePackages = "io.j13n")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CoreApplication {
